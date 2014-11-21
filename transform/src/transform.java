c3a9d9899043785bcdf0030e3504040f9c2c5beb
s-xuch <s-xuch@54L1808-11.it.bsd405.org>
2014-11-20 16:00:05 -0800

    added some checkers

--- transform/src/transform.java
+++ transform/src/transform.java
@@ -3,6 +3,7 @@
  TASK: transform
  LANG: JAVA
  */
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -13,35 +14,85 @@ import java.util.StringTokenizer;
 
 public class transform {
 
+    public static BufferedReader in;
+    public static PrintWriter out;
+    public static int size;
+    public static boolean[][] square1;
+    public static boolean[][] square2;
+
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        BufferedReader in = new BufferedReader(new FileReader("transform.in"));
-        PrintWriter out = new PrintWriter(new FileWriter("transform.out"));
+        in = new BufferedReader(new FileReader("transform.in"));
+        out = new PrintWriter(new FileWriter("transform.out"));
         String reader;
-        int size = Integer.parseInt(in.readLine());
-        boolean[][] square1 = new boolean[size][size];
-        boolean[][] square2 = new boolean[size][size];
+        size = Integer.parseInt(in.readLine());
+        square1 = new boolean[size][size];
+        square2 = new boolean[size][size];
         for (int counterY = 0; counterY < size; counterY++) {
             reader = in.readLine();
             for (int counterX = 0; counterX < size; counterX++) {
-                if (reader.charAt(counterX) == '@') {
-                    square1[counterY][counterX] = true;
-                } else {
-                    square1[counterY][counterX] = false;
-                }
+                square1[counterY][counterX] = reader.charAt(counterX) == '@';
             }
         }
         for (int counterY = 0; counterY < size; counterY++) {
             reader = in.readLine();
             for (int counterX = 0; counterX < size; counterX++) {
-                if (reader.charAt(counterX) == '@') {
-                    square2[counterY][counterX] = true;
-                } else {
-                    square2[counterY][counterX] = false;
-                }
+                square2[counterY][counterX] = reader.charAt(counterX) == '@';
             }
         }
 
         out.close();
         System.exit(0);
+    }
+
+    static boolean check90() {
+        boolean status = true;
+        return status;
+    }
+
+    static boolean check180() {
+        boolean status = true;
+        return status;
+    }
+
+    static boolean check270() {
+        boolean status = true;
+        return status;
+    }
+
+    static boolean checkXReflection() {
+        boolean status = true;
+        for (int counterY = 0; counterY < size && status; counterY++) {
+            for (int counterX = 0; counterX < Math.round(size / 2); counterX++) {
+                if (!((square1[counterY][counterX] && square2[counterY][size - 1 - counterX])
+                        || !(square1[counterY][counterX] && square2[counterY][size - 1 - counterX]))) {
+                    status = false;
+                }
+            }
+        }
+        return status;
+
+    }
+
+    static boolean checkYReflection() {
+        boolean status = true;
+        for (int counterX = 0; counterX < size && status; counterX++) {
+            for (int counterY = 0; counterY < Math.round(size / 2); counterY++) {
+                if (!((square1[counterY][counterX] && square2[size - 1 - counterY][counterX])
+                        || !(square1[counterY][counterX] && square2[size - 1 - counterY][counterX]))) {
+                    status = false;
+                }
+            }
+        }
+        return status;
+    }
+
+    static boolean[][] rotate90(boolean[][] array) {
+        int size = array.length;
+        for (int counterY = 0; counterY < size; counterY++) {
+            for (int counterX = 0; counterX < size; counterX++) {
+                
+            }
+        }
+        return array;
     }
 }
