db236683fb613e7a19c255c8535a5744f039f8ad
Peachball <chen_xu@live.com>
2014-12-04 19:41:19 -0800

    Solved

--- namenum/src/namenum.java
+++ namenum/src/namenum.java
@@ -3,17 +3,15 @@
  TASK: namenum
  LANG: JAVA
  */
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
 
 public class namenum {
 
@@ -27,8 +25,7 @@ public class namenum {
         BufferedReader in = new BufferedReader(new FileReader("namenum.in"));
         PrintWriter out = new PrintWriter(new FileWriter("namenum.out"));
         BufferedReader dictionary = new BufferedReader(new FileReader("dict.txt"));
-        namenum instance = new namenum();
-        String[] names;
+        ArrayList<String> names = new ArrayList<String>();
         dictNames = readDictionary(dictionary);
 
         String buffer = in.readLine();
@@ -37,29 +34,52 @@ public class namenum {
         for (int counter = 0; counter < size; counter++) {
             serialNum[counter] = (short) Integer.parseInt(buffer.charAt(counter) + "");
         }
-//        names = instance.nameGen2(serialNum);
-//        for (int counter = 0; counter < names.length; counter++) {
-//            if (isElementInArray(dictNames, names[counter])) {
-//                validNames.add(names[counter]);
-//            }
-//        }
-//        if (validNames.isEmpty()) {
-//            out.println("NONE");
-//            System.out.println("NONE");
-//        } else {
-//            String[] newNames = new String[validNames.size()];
-//            validNames.toArray(newNames);
-//            Arrays.sort(newNames);
-//            for (int counter = 0; counter < newNames.length; counter++) {
-//                out.println(newNames[counter]);
-//                System.out.println(newNames[counter]);
-//            }
-//        }
-        
-        
-        
+        //Convert Dictionary name into number.
+        for (int counter = 0; counter < dictNames.length; counter++) {
+
+            short[] buffer2 = new short[dictNames[counter].length()];
+
+            //Loop to convert each char into a short for the buffer
+            for (int counter2 = 0; counter2 < dictNames[counter].length(); counter2++) {
+
+                //Loop to determine which character to check
+                for (int counter5 = 0; counter5 < dictNames[counter].length(); counter5++) {
+
+                    //Another loop to figure out which number each letter is...
+                    //loop to figure out keypad position
+                    for (int counter3 = 2; counter3 < keypad.length; counter3++) {
+
+                        //Second loop to check the letter
+                        for (int counter4 = 0; counter4 < 3; counter4++) {
+                            if (keypad[counter3][counter4] == dictNames[counter].charAt(counter5)) {
+                                buffer2[counter5] = (short) counter3;
+                            }
+                        }
+                    }
+                }
+            }
+            if (Arrays.equals(serialNum, buffer2)) {
+                names.add(dictNames[counter]);
+            }
+        }
+
+        //Print out results
+        if (names.size() > 0) {
+            for (int counter = 0; counter < names.size(); counter++) {
+                out.println(names.get(counter));
+                System.out.println(names.get(counter));
+            }
+        } else {
+            out.println("NONE");
+            System.out.println("NONE");
+        }
         out.close();
         System.exit(0);
+    }
+
+    static boolean isElementInArray(String[] stringray, String element) {
+        int intdex = Arrays.binarySearch(stringray, element);
+        return (intdex >= 0);
     }
 
     static String[] readDictionary(BufferedReader input) throws IOException {
@@ -67,29 +87,17 @@ public class namenum {
         LinkedList<String> names = new LinkedList<String>();
         int counter = 0;
         buffer = input.readLine();
-        while (!(buffer == null)) {//Copy the dict.txt into the dictionary
+        while (!(buffer == null)) {   //Copy the dict.txt into the dictionary
             names.add(buffer);
             buffer = input.readLine();
             counter++;
         }
         String[] output = new String[names.size()];
         names.toArray(output);
-        
+
         //Put in loop that converts output to numbers
         // Achievment Get: Make Brennan Ragequit
-        for (int i = 0; i < output.length; i++){
-            String sTemp = output[i];
-
-            
-        }
-        
         return output;
-    }
-    
-    static boolean checkName(String element, short[] serialnum ){
-        
-        
-        return true;
     }
 //    String[] nameGenerator(short[] serialnum, int position) {
 //        ArrayList<String> names = new ArrayList<String>();
@@ -146,41 +154,36 @@ public class namenum {
 //        valids.toArray(names);
 //        return names;
 //    }
-
-    static boolean isElementInArray(String[] stringray, String element) {
-        int intdex = Arrays.binarySearch(stringray, element);
-        return (intdex >= 0);
-    }
-/*
-//    static boolean isElementInArray(String[] array, String element) {
-//        int position1 = 0;
-//        int position2 = array.length;
-//        int status = 0;
-//        boolean alt = true;
-//       
-//        while (position1 != position2 && !(Math.abs(position1 - position2) == 1
-//                && compare(element, array[position1]) != 0
-//                && compare(element, array[position2]) != 0)) {
-//            if(alt){
-//            counter = (int) (position1 + position2) / 2;
-//            } else{
-//                
-//            }
-//            switch (status) {
-//                case 0:
-//                    return true;
-//                case 1:
-//                    position1 = counter;
-//                    alt = true;
-//                    break;
-//                case 2:
-//                    position2 = counter;
-//                    alt = false;
-//                    break;
-//            }
-//        } // true is position1
-//          // false is poisition2
-//        return false;
-//    }
-*/
+    /*
+     //    static boolean isElementInArray(String[] array, String element) {
+     //        int position1 = 0;
+     //        int position2 = array.length;
+     //        int status = 0;
+     //        boolean alt = true;
+     //       
+     //        while (position1 != position2 && !(Math.abs(position1 - position2) == 1
+     //                && compare(element, array[position1]) != 0
+     //                && compare(element, array[position2]) != 0)) {
+     //            if(alt){
+     //            counter = (int) (position1 + position2) / 2;
+     //            } else{
+     //                
+     //            }
+     //            switch (status) {
+     //                case 0:
+     //                    return true;
+     //                case 1:
+     //                    position1 = counter;
+     //                    alt = true;
+     //                    break;
+     //                case 2:
+     //                    position2 = counter;
+     //                    alt = false;
+     //                    break;
+     //            }
+     //        } // true is position1
+     //          // false is poisition2
+     //        return false;
+     //    }
+     */
 }
