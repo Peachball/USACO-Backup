7c530af3687908eb65e021ce3a58c693c6716973
Peachball <chen_xu@live.com>
2014-12-17 20:46:57 -0800

    An attempt to make myself feel better over the fact that I missed a USACO competition through purely being stupid

---
+++ Competition Problems/Odometer/src/odometer.java
@@ -0,0 +1,21 @@
+/*
+ ID: chen_xu1
+ TASK: odometer
+ LANG: JAVA
+ */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class odometer {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new FileReader("odometer.in"));
+        PrintWriter out = new PrintWriter(new FileWriter("odometer.out"));
+        
+        out.close();
+        System.exit(0);
+    }
+}
