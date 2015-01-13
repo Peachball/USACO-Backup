731800aae745adc1d8524fba7984c1dd4bad361f
Peachball <chen_xu@live.com>
2015-01-12 18:25:54 -0800

    Sucessfully read from a file!

--- Crypt1/main.c
+++ Crypt1/main.c
@@ -7,20 +7,37 @@ LANG: C
 #include <stdio.h>
 #include <stdlib.h>
 
+int digits(const int x){
+    int buffer =x;
+    int counter = 1;
+    while(buffer>9){
+        buffer = buffer/10;
+        counter++;
+    }
+    return counter;
+}
+
 int main(int argc, char** argv) {
     FILE *in, *out;
-    in =fopen("crypt1.in", "r");
-    out =fopen( "crypt1.out", "w");
+    in = fopen("crypt1.in", "r");
+    out = fopen("crypt1.out", "w");
 
     //read input here
     int numofInput;
+    int buffer;
     int counter = 0;
-    fscanf(in, "%d\n", &numofInput);
-    char buffer[10];
-    fscanf(in,"%s",buffer);
+    fscanf(in, "%d", &numofInput);
+    int digits[numofInput];
     for (counter = 0; counter < numofInput; counter++) {
-        printf("%c", buffer[counter]);
+        buffer = getc(in);
+        if (buffer - '0' < 0 || buffer - '0' > 9) {
+            counter--;
+        } else {
+            digits[counter] = buffer - '0';
+        }
     }
+    
+    
 
     return (EXIT_SUCCESS);
 }
