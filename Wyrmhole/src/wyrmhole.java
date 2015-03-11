884ce58241798aa3f46dfb037e6d99ec10a08ac8
s-xuch <s-xuch@54L1808-07.it.bsd405.org>
2015-03-11 15:40:59 -0700

    Recursion Attempt 1

--- Wyrmhole/src/wyrmhole.java
+++ Wyrmhole/src/wyrmhole.java
@@ -10,6 +10,9 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class wyrmhole {
@@ -22,28 +25,66 @@ public class wyrmhole {
         Coord input[] = new Coord[max];
         for (int counter = 0; counter < max; counter++) {
             StringTokenizer read = new StringTokenizer(in.readLine());
+            input[counter] = new Coord();
             input[counter].x = Integer.parseInt(read.nextToken());
             input[counter].y = Integer.parseInt(read.nextToken());
         }
 
-        for(int counter = 0;;counter++){
-            Wormhole wormholes[] = new Wormhole[max];
-            
-        }
+        //Generate all possiblities?
+        wyrmhole a = new wyrmhole();
+        LinkedList<ArrayList<Coord>> possiblities = a.recursiondonhurtme(new ArrayList<Coord>(Arrays.asList(input)));
+
         out.close();
         System.exit(0);
     }
+
+    public LinkedList<ArrayList<Coord>> recursiondonhurtme(ArrayList<Coord> input) {
+        LinkedList<ArrayList<Coord>> buffer = new LinkedList<ArrayList<Coord>>();
+        if (input.size() == 2) {
+            input.get(0).link(input.get(1));
+            input.get(1).link(input.get(0));
+            buffer.add(input);
+            return buffer;
+        }
+        ArrayList<Coord> buffer3;
+        for (int counter = 1; counter < input.size(); counter++) {
+            buffer3 = input;
+            buffer3.get(0).link(input.get(counter));
+            buffer3.get(counter).link(input.get(0));
+            Coord[] buffer2 = new Coord[2];
+            buffer2[0] = buffer3.get(0);
+            buffer2[1] = buffer3.get(counter);
+            buffer3.remove(0);
+            buffer3.remove(counter);
+            LinkedList<ArrayList<Coord>> buffer4 = recursiondonhurtme((ArrayList<Coord>)buffer3.clone()) ;
+            for(int counter2 = 0;counter2<buffer4.size();counter2++){
+                buffer4.get(counter2).add(buffer2[0]);
+                buffer4.get(counter2).add(buffer2[1]);
+            }
+            buffer.addAll(buffer4);
+        }
+        return buffer;
+    }
 }
 
-class /* STRUCT */ Wormhole {
+class /* STRUCT */ Coord {
 
-    public Coord input;
-    public Coord output;
-}
-class /* STRUCT */ Coord{
     public int x;
     public int y;
-    public Coord(){
-        
+    public int friendx;
+    public int friendy;
+
+    public Coord(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
+
+    public Coord() {
+    }
+
+    public void link(Coord friend) {
+        this.friendx = friend.x;
+        this.friendy = friend.y;
+    }
+
 }
